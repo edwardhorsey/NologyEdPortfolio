67ba3f3dee560f15e366dbb8375f5115b8b13e41
Edward Horsey <edwardhorsey@me.com>
2020-05-15 12:46:44 +0100

    arrow keys working

--- index.js
+++ index.js
@@ -1,40 +1,31 @@
 //jquery
 
 
-
-
 // arrow keys to navigate
 console.log(
     document.URL
 )
 
-// document.addEventListener("keydown", e => {
-//     if (e.keyCode === 39) { console.log('hi') }
-// });
-
-
-
-document.addEventListener("keydown", e => { goToPage(e) })
-
+document.addEventListener("keydown", e => {
+    if (e.keyCode === 37 || e.keyCode === 39) {
+        e.preventDefault();
+        goToPage(e);
+    }
+});
 
 function goToPage(key) {
     let location = document.URL;
-    let section = location.slice(location.indexOf('#'))
+    let section = location.slice(location.indexOf('#') + 1)
 
-    let left = document.querySelector(`${section} > div > div.icons-bar > ul > li.left > a > img`)
-    let right = document.querySelector(`${section} > div > div.icons-bar > ul > li.right > a > img`)
+    let left = document.querySelector(`.${section} .icons-bar .left a`)
+    let right = document.querySelector(`.${section} .icons-bar .right a`)
 
-    // let left = document.querySelector(`${section} > div > div:nth-child(2) > div > ul > li.right > a`) // the ones for skills
-    // let right = document.querySelector(`${section} > div > div:nth-child(2) > div > ul > li.right > a`)
     console.log(left, right)
 
     if (key.keyCode === 39) { return right.click() }
-
     if (key.keyCode === 37) { return left.click() }
 
 }
-
-goToPage(123)
 
 // contact form thanks
 
@@ -45,6 +36,5 @@ function thanks() {
         para.innerText = 'Thanks!'
     }
 }
-
 
 // colours for error on form
